a4b849a98889e74f815208d23936dcd9348da53e
Neelj626 <neel.jhangiani1@gmail.com>
2024-11-11 15:10:49 -0600

    initial commit

---
+++ portfolio-website/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,47 @@
+import React, {useState} from 'react'
+
+import styles from './Navbar.module.css';
+
+import home_button from '../../Assets/nav/Home_button.png';
+import menu_icon from '../../Assets/nav/menu_icon.png';
+import close_icon from '../../Assets/nav/close_icon.png';
+
+
+export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className={styles.navbar}>
+        
+        <a className={styles.home} href="#home">
+            <img src={home_button} alt=''/>
+        </a>
+        
+        <div className={styles.menu}>
+            <img 
+                className={styles.menuBtn} 
+                src={menuOpen ? close_icon : menu_icon} 
+                alt=""
+                onClick={() =>setMenuOpen(!menuOpen)}
+            />
+            <ul 
+                className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+                onClick={() => setMenuOpen(false)}
+            >
+                <li>
+                    <a href="#about">About</a>
+                </li>
+                <li>
+                    <a href="#skills">Skills</a>
+                </li>
+                <li>
+                    <a href="#projects">Projects</a>
+                </li>
+                <li>
+                    <a href="#contact">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+  );
+};
