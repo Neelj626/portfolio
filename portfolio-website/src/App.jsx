3d9073afc7ea079029d9bf1befcc7b4bf4c06004
Neelj626 <neel.jhangiani1@gmail.com>
2024-11-11 17:11:25 -0600

    update home section

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -1,9 +1,11 @@
 import styles from "./App.module.css";
+import { Home } from "./Components/Home/Home";
 import { Navbar } from "./Components/Navbar/Navbar";
 
 function App() {
   return <div className={styles.App}>
     <Navbar />
+    <Home />
   </div>;
 }
 
---
+++ portfolio-website/src/Components/Home/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styles from './Home.module.css';
+
+import car_img from '../../Assets/home/car.png';
+
+export const Home = () => {
+  return (
+    <section className={styles.container}>
+        <div className={styles.content}>
+            <h1 className={styles.title}> Hello, I'm Neel Jhangiani</h1>
+            <p className={styles.description}>
+                Software Developer | 
+                Data Science & Machine Learning Enthusiast
+            </p>
+
+            <a 
+                href="mailto:neel.jhangiani1@gmail.com" 
+                className={styles.contactBtn}> Contact
+            </a>
+        </div>
+
+        <img src={car_img} alt="" className={styles.carImg} />
+
+        <div className={styles.topBlur} />
+        <div className={styles.bottomBlur} />
+
+    </section>
+  );
+};
